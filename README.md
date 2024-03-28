be5c58b4fad2a4b51170edc251cfda65ec35137b
xnkjj <16268065+xnkjj@users.noreply.github.com>
2024-03-28 16:57:58 +0000

    feat: add secret

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ```bash
-curl ${YOURDOMAIN}/$(curl https://ipinfo.io/ip -s)
+docker run --name myip -d -p 80:80 -e "SECRET=your_secret" xnkjj/myip
 ```
 
 ```bash
-ssh root@$(curl ${YOURDOMAIN} -s)
+curl ${YOURDOMAIN}/$(curl https://ipinfo.io/ip -s) -H "SECRET:your_secret"
+```
+
+```bash
+ssh root@$(curl ${YOURDOMAIN} -s -H "SECRET:your_secret")
 ```
--- main.go
+++ main.go
@@ -1,32 +1,44 @@
 package main
 
 import (
+	"os"
 	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
 
-var myip string = ""
+var (
+	myip   string = ""
+	secret string = os.Getenv("SECRET")
+)
 
 const reg = `^(\d+).(\d+).(\d+).(\d+)$`
 
 func main() {
-	r := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
 	gin.DisableConsoleColor()
+	r := gin.Default()
+	r.SetTrustedProxies([]string{gin.PlatformCloudflare})
 
 	r.GET("/put/:ip", func(ctx *gin.Context) {
+		if secret != ctx.GetHeader("SECRET") {
+			ctx.String(403, "Forbidden")
+			return
+		}
 		ip := ctx.Param("ip")
 		if regexp.MustCompile(reg).MatchString(ip) {
 			myip = ip
-			ctx.String(200, myip)
-		} else {
-			ctx.String(200, myip)
 		}
-	})
-
-	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(200, myip)
 	})
 
-	r.Run(":80")
+	r.GET("/", func(ctx *gin.Context) {
+		if secret == ctx.GetHeader("SECRET") {
+			ctx.String(200, myip)
+		} else {
+			ctx.String(200, ctx.ClientIP())
+		}
+	})
+
+	r.Run(":8088")
 }
